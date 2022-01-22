9fa941c2f4ad1b144a64f9bd003cdb3f2f4f930f
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-01-22 13:21:42 +0530

    template inheritance added

--- app.js
+++ app.js
@@ -15,7 +15,11 @@ app.set('views',path.join(__dirname,'views'))
 //endpoint
 app.get('/',(req,res)=>{
     const params= {}  
-    res.status(200).render('index.pug',params)
+    res.status(200).render('home.pug',params)
+})
+app.get('/contact',(req,res)=>{
+    const params= {}  
+    res.status(200).render('contact.pug',params)
 })
 
 
