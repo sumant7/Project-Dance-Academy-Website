1522358a6b51037688b9a44c6b254e1fa6c4d87c
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-01-27 20:03:58 +0530

    backend oc contact completed

--- app.js
+++ app.js
@@ -3,6 +3,24 @@ const fs = require("fs");
 const path= require("path")
 const app = express();
 const port = 80;
+const bodyparser= require("body-parser")
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/contactDance',{useNewUrlParser: true});
+
+
+//define mongoose schema
+var contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    email: String
+});
+
+var Contact = mongoose.model('Contact', contactSchema);
+
+
 
 //express
 app.use('/static',express.static('static'))
@@ -20,6 +38,15 @@ app.get('/',(req,res)=>{
 app.get('/contact',(req,res)=>{
     const params= {}  
     res.status(200).render('contact.pug',params)
+})
+app.post('/contact',(req,res)=>{
+    myData= new Contact(req.body)
+    myData.save().then(()=>{
+        res.send("Saved to database")
+    }).catch(()=>{
+        res.status(400).send("Not Saved")
+    });
+    var myData= new Contact(req.body);  
 })
 
 
